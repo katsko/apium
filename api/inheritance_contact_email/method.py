061c6d0d338bf1e9a336bbc587be25c8e3cfe1bf
Alexandr Katsko <alexandr.katsko@gmail.com>
2020-07-13 23:52:39 +0700

    draft for inheritance

---
+++ api/inheritance_contact_email/method.py
@@ -0,0 +1,20 @@
+from jrlib import jf
+from api_base.contact.base_method import BaseContact
+
+
+class InheritanceContactEmail(BaseContact):
+    email = jf.Email()
+
+    def execute(self):
+        return {
+            'user_id': self.user_id,
+            'comment': self.comment,
+            'email': self.email,
+        }
+
+    def validate(self):
+        super(InheritanceContactEmail, self).validate()
+        print(self.email)
+        print(self.user_id)
+        if not self.email or len(self.email) != self.user_id:
+            raise ValueError('Error email lenght')
---
+++ api/inheritance_contact_phone/method.py
@@ -0,0 +1,18 @@
+from jrlib import jf
+from api_base.contact.base_method import BaseContact
+
+
+class InheritanceContactPhone(BaseContact):
+    phone = jf.Str()
+
+    def execute(self):
+        return {
+            'user_id': self.user_id,
+            'comment': self.comment,
+            'phone': self.phone,
+        }
+
+    def validate(self):
+        super(InheritanceContactPhone, self).validate()
+        if not self.phone or len(self.phone) != self.user_id:
+            raise ValueError('Error phone lenght')
